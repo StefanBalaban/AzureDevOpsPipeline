e27b3fa0e4ac743a858095a5732bd85dd5f25517
Stefan Balaban <stefanbalaban97@gmail.com>
2019-09-14 14:33:00 +0200

    Dodao digitron klasu

---
+++ WebApp/Digitron.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class Digitron
+    {
+        public int Saberi(int a, int b) => a + b;
+        public int Oduzmi(int a, int b) => a - b;
+    }
+}
