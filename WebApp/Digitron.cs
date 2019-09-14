1c30668fa59f80bcb13d0f869850b4671c262e49
Stefan Balaban <stefanbalaban97@gmail.com>
2019-09-14 14:57:44 +0200

    Oduzmi metodu Digitron klase izmjenio tako da test case vrati fail

--- WebApp/Digitron.cs
+++ WebApp/Digitron.cs
@@ -8,6 +8,6 @@ namespace WebApp
     public class Digitron
     {
         public int Saberi(int a, int b) => a + b;
-        public int Oduzmi(int a, int b) => a - b;
+        public int Oduzmi(int a, int b) => a;
     }
 }
