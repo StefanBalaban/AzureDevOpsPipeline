78b2f59ea210df0a648be5e03d3fe63dc2586ff9
Stefan Balaban <stefanbalaban97@gmail.com>
2019-09-14 14:46:25 +0200

    Dodao unit testove za Saberi metodu Digitron klase

---
+++ WebAppUnitTests/DigitronTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApp;
+
+namespace WebAppUnitTests
+{
+    [TestFixture]
+    class DigitronTests
+    {
+        [Test]
+        public void Saberi_PozvanSaDvaDva_VratiCetiri()
+        {
+            var digitron = new Digitron();
+
+            var cetiri = digitron.Saberi(2, 2);
+
+            Assert.That(cetiri, Is.EqualTo(4));
+        }
+    }
+}
